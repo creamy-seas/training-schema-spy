598e6d271df705de9ff6e5c7f2a951515d5b025e
Ilya Antonov <ilya.antonov@dreams-ai.com>
2020-04-07 12:00:42 +0000

    Update generate_schema.sh

--- generate_schema.sh
+++ generate_schema.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-# 0 - config
 PROP_FILE=$1
 if [ -z $1 ]; then
     echo "Need to supply the *.properties file"
     exit
-fi
-
-# 1 - pull in schemaspy if required
-if [ -z $(docker images | grep schemaspy/schemaspy) ]; then
-    docker pull schemaspy/schemaspy
 fi
 
 WORKING_DIRECTORY=$(pwd)
@@ -17,9 +11,8 @@ echo "Output mapping: $WORKING_DIRECTORY/output:/output"
 echo "Driver mapping: $WORKING_DIRECTORY/drivers:/drivers"
 echo "Property mapping: $WORKING_DIRECTORY/$PROP_FILE:/schemaspy.properties"
 
-# 3 - run with the supplied file
 docker run \
        -v "$WORKING_DIRECTORY/output:/output" \
        -v "$WORKING_DIRECTORY/$PROP_FILE:/schemaspy.properties" \
        -v "$WORKING_DIRECTORY/drivers:/drivers" \
-       schemaspy/schemaspy:latest
+       schemaspy/schemaspy:snapshot
